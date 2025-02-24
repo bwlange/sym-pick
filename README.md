fadf347d3c0d2f35e084ddb2cda742f31ee3ca87
bwlange <104918789+bwlange@users.noreply.github.com>
2025-02-24 08:34:46 -0600

    initial commit: add Sym-Pick VS Code extension with basic configuration and files.

---
+++ README.md
@@ -0,0 +1,42 @@
+# Sym-Pick VS Code Extension
+
+## Overview
+
+The Sym-Pick extension for Visual Studio Code enhances your development experience. This extension helps you insert Unicode symbols into your code comments, enhancing them visually.
+
+## Features
+
+- **Symbol Autocomplete**: Allows you to type ":" inside a string or comment block triggering an autocomplete drop down that allows you to select the symbol you desire.
+
+
+## Installation
+
+1. Open Visual Studio Code.
+2. Go to the Extensions view by clicking on the Extensions icon in the Activity Bar on the side of the window.
+3. Search for `Sym-Pick`.
+4. Click `Install` to install the extension.
+5. Reload Visual Studio Code to activate the extension.
+
+## Usage
+
+1. Open a project or create a new one in Visual Studio Code.
+2. Ensure the Sym-Pick extension is activated.
+3. Start coding with symbol selection feature.
+
+## Contributing
+
+I welcome contributions to the Sym-Pick extension. To contribute:
+
+1. Fork the repository.
+2. Create a new branch for your feature or bug fix.
+3. Make your changes and commit them with clear messages.
+4. Push your changes to your fork.
+5. Submit a pull request.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+## Contact
+
+For any questions or feedback, please contact me at [bwlange@gmail.com](mailto:bwlange@gmail.com).
---
+++ src/symbols.ts
@@ -0,0 +1,357 @@
+export const SYMBOLS: Record<string, { prefix: string; symbol: string; description: string }> = {
+    sparkles: {
+        prefix: ":sparkles",
+        symbol: "✨",
+        description: "✨ Sparkles"
+    },
+    star: {
+        prefix: ":star",
+        symbol: "🌟",
+        description: "🌟 Star"
+    },
+    heart: {
+        prefix: ":heart",
+        symbol: "💖",
+        description: "💖 Heart"
+    },
+    rainbow: {
+        prefix: ":rainbow",
+        symbol: "🌈",
+        description: "🌈 Rainbow"
+    },
+    fourleaf_clover: {
+        prefix: ":four-leaf_clover",
+        symbol: "🍀",
+        description: "🍀 Four-leaf Clover"
+    },
+    party_popper: {
+        prefix: ":party_popper",
+        symbol: "🎉",
+        description: "🎉 Party Popper"
+    },
+    lightbulb: {
+        prefix: ":lightbulb",
+        symbol: "💡",
+        description: "💡 Lightbulb (for ideas)"
+    },
+    fire: {
+        prefix: ":fire",
+        symbol: "🔥",
+        description: "🔥 Fire (hot features)"
+    },
+    errors: {
+        prefix: ":errors",
+        symbol: "🔥",
+        description: "🔥 errors"
+    },
+    pushpin: {
+        prefix: ":pushpin",
+        symbol: "📌",
+        description: "📌 Pushpin (important notes)"
+    },
+    rocket: {
+        prefix: ":rocket",
+        symbol: "🚀",
+        description: "🚀 Rocket (fast performance)"
+    },
+    export: {
+        prefix: ":export",
+        symbol: "🚀",
+        description: "🚀 Export"
+    },
+    loudspeaker: {
+        prefix: ":loudspeaker",
+        symbol: "📢",
+        description: "📢 Loudspeaker (announcement)"
+    },
+    callout: {
+        prefix: ":callout",
+        symbol: "📢",
+        description: "📢 callout"
+    },
+    important_alert: {
+        prefix: ":important_alert",
+        symbol: "❗",
+        description: "❗ Important alert"
+    },
+    warning: {
+        prefix: ":warning",
+        symbol: "⚠️",
+        description: "⚠️ Warning"
+    },
+    stop_sign: {
+        prefix: ":stop_sign",
+        symbol: "🛑",
+        description: "🛑 Stop sign (critical issue)"
+    },
+    work_in_progress: {
+        prefix: ":work_in_progress",
+        symbol: "🚧",
+        description: "🚧 Work in progress"
+    },
+    search: {
+        prefix: ":search",
+        symbol: "🔍",
+        description: "🔍 Search"
+    },
+    find: {
+        prefix: ":find",
+        symbol: "🔍",
+        description: "🔍 find"
+    },
+    filter: {
+        prefix: ":filter",
+        symbol: "🔍",
+        description: "🔍 filter"
+    },
+    construction: {
+        prefix: ":construction",
+        symbol: "🏗",
+        description: "🏗 Construction (in development)"
+    },
+    notepad: {
+        prefix: ":notepad",
+        symbol: "📝",
+        description: "📝 Notepad (notes)"
+    },
+    todos: {
+        prefix: ":todos",
+        symbol: "📝",
+        description: "📝 TODOs"
+    },
+    documentation: {
+        prefix: ":documentation",
+        symbol: "📝",
+        description: "📝 documentation"
+    },
+    wrench: {
+        prefix: ":wrench",
+        symbol: "🛠",
+        description: "🛠 Wrench fixes"
+    },
+    setup: {
+        prefix: ":setup",
+        symbol: "🛠",
+        description: "🛠 setup"
+    },
+    tools: {
+        prefix: ":tools",
+        symbol: "🛠",
+        description: "🛠 tools"
+    },
+    success: {
+        prefix: ":success",
+        symbol: "✅",
+        description: "✅ Success"
+    },
+    complete: {
+        prefix: ":complete",
+        symbol: "✅",
+        description: "✅ complete"
+    },
+    exception: {
+        prefix: ":exception",
+        symbol: "🔥",
+        description: "🔥 Exception"
+    },
+    failure: {
+        prefix: ":failure",
+        symbol: "🔥",
+        description: "🔥 failure"
+    },
+    error: {
+        prefix: ":error",
+        symbol: "🔥",
+        description: "🔥 error"
+    },
+    loop: {
+        prefix: ":loop",
+        symbol: "🔄",
+        description: "🔄 Loop"
+    },
+    iterate: {
+        prefix: ":iterate",
+        symbol: "🔄",
+        description: "🔄 iterate"
+    },
+    refresh: {
+        prefix: ":refresh",
+        symbol: "🔄",
+        description: "🔄 refresh"
+    },
+    retry: {
+        prefix: ":retry",
+        symbol: "🔄",
+        description: "🔄 retry"
+    },
+    sync: {
+        prefix: ":sync",
+        symbol: "⏳",
+        description: "⏳ Sync"
+    },
+    async: {
+        prefix: ":async",
+        symbol: "⏳",
+        description: "⏳ async"
+    },
+    time: {
+        prefix: ":time",
+        symbol: "⏳",
+        description: "⏳ time"
+    },
+    folder: {
+        prefix: ":folder",
+        symbol: "📂",
+        description: "📂 Folder"
+    },
+    directory: {
+        prefix: ":directory",
+        symbol: "📂",
+        description: "📂 directory"
+    },
+    package: {
+        prefix: ":package",
+        symbol: "📦",
+        description: "📦 Package"
+    },
+    payload: {
+        prefix: ":payload",
+        symbol: "📦",
+        description: "📦 Payload"
+    },
+    link: {
+        prefix: ":link",
+        symbol: "🔗",
+        description: "🔗 Link"
+    },
+    url: {
+        prefix: ":url",
+        symbol: "🔗",
+        description: "🔗 URL"
+    },
+    system: {
+        prefix: ":system",
+        symbol: "🖥",
+        description: "🖥 System"
+    },
+    build: {
+        prefix: ":build",
+        symbol: "🔧",
+        description: "🔧 Build(s)"
+    },
+    configuration: {
+        prefix: ":configuration",
+        symbol: "🔧",
+        description: "🔧 Configuration"
+    },
+    endpoint: {
+        prefix: ":endpoint",
+        symbol: "🌐",
+        description: "🌐 Endpoint"
+    },
+    api: {
+        prefix: ":api",
+        symbol: "🌐",
+        description: "🌐 API"
+    },
+    connection: {
+        prefix: ":connection",
+        symbol: "🔗",
+        description: "🔗 connection"
+    },
+    http_post: {
+        prefix: ":http_post",
+        symbol: "📡",
+        description: "📡 HTTP POST"
+    },
+    http_get: {
+        prefix: ":http_get",
+        symbol: "📡",
+        description: "📡 HTTP GET"
+    },
+    http_request: {
+        prefix: ":http_request",
+        symbol: "📡",
+        description: "📡 HTTP Request"
+    },
+    fast_response: {
+        prefix: ":fast_response",
+        symbol: "⚡",
+        description: "⚡ Fast response"
+    },
+    microservice: {
+        prefix: ":microservice",
+        symbol: "⚡",
+        description: "⚡ microservice"
+    },
+    performance_boost: {
+        prefix: ":performance_boost",
+        symbol: "🚀",
+        description: "🚀 Performance boost"
+    },
+    incoming_request: {
+        prefix: ":incoming_request",
+        symbol: "📥",
+        description: "📥 Incoming request"
+    },
+    outgoing_response: {
+        prefix: ":outgoing_response",
+        symbol: "📤",
+        description: "📤 Outgoing response"
+    },
+    api_gateway: {
+        prefix: ":api_gateway",
+        symbol: "🌐",
+        description: "🌐 API gateway"
+    },
+    contract: {
+        prefix: ":contract",
+        symbol: "📜",
+        description: "📜 Contract(s)"
+    },
+    fee: {
+        prefix: ":fee",
+        symbol: "💲",
+        description: "💲 Fee"
+    },
+    mulesoft: {
+        prefix: ":mulesoft",
+        symbol: "🐴",
+        description: "🐴 MuleSoft"
+    },
+    list: {
+        prefix: ":list",
+        symbol: "📋",
+        description: "📋 List"
+    },
+    alert: {
+        prefix: ":alert",
+        symbol: "❗",
+        description: "❗ Alert"
+    },
+    target: {
+        prefix: ":target",
+        symbol: "🎯",
+        description: "🎯 Target"
+    },
+    goal: {
+        prefix: ":goal",
+        symbol: "🎯",
+        description: "🎯 goal"
+    },
+    integration: {
+        prefix: ":integration",
+        symbol: "🚀",
+        description: "🚀 integration"
+    },
+    stop: {
+        prefix: ":stop",
+        symbol: "🛑",
+        description: "🛑 Stop"
+    },
+    notes: {
+        prefix: ":notes",
+        symbol: "📌",
+        description: "📌 Notes"
+    }
+}
